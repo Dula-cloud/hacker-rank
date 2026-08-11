78aa1c9ece2972af432ee004716e34be90afc4c9
Dulangana Rupasinghe <127759019+Dula-cloud@users.noreply.github.com>
2026-08-11 23:34:30 +0530

    Update Solution.java

--- java/java string tokens/Solution.java
+++ java/java string tokens/Solution.java
@@ -12,7 +12,6 @@ public class Solution {
         String b=  "Sample";dda
         String c=  "Text";werwrd
         String c=  "23t";
-        jkkkkl;k mlmlk
         StringBuilder sb = (StringBuilder)a;
         // Write your code here.
         scan.close();
