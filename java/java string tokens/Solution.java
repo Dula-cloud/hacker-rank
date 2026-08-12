d6ffbddf07255d69a58072f267ca30b3e5086fc6
Dulangana Rupasinghe <127759019+Dula-cloud@users.noreply.github.com>
2026-08-12 17:20:30 +0530

    Update Solution.java

--- java/java string tokens/Solution.java
+++ java/java string tokens/Solution.java
@@ -6,9 +6,24 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
-        System.out.println(s);
-        // Write your code here.
+
+        String[] words = s.split("[^A-Za-z]+");
+
+        int count = 0;
+
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                count++;
+            }
+        }
+        System.out.println(count);
+
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                System.out.println(word);
+            }
+        }
+
         scan.close();
     }
 }
-
