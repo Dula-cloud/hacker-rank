b88ba03b19f84ddf781f7c69a71f97b495d087f8
Dulangana Rupasinghe <127759019+Dula-cloud@users.noreply.github.com>
2026-08-12 19:25:09 +0530

    Update Solution.java

--- java/java pattern syntax checker/Solution.java
+++ java/java pattern syntax checker/Solution.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
 import java.util.regex.*;
 
-public class Solution
-{
-	public static void main(String[] args){
-		Scanner in = new Scanner(System.in);
-		int testCases = Integer.parseInt(in.nextLine());
-		while(testCases>0){
-			String pattern = in.nextLine();
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while (testCases > 0) {
+            if (in.hasNextLine()) {
+                String pattern = in.nextLine();
 
-            if(isValidPatter(pattern)){
-                System.out.println("Valid");
-            }else{
-                System.out.println("Invalid");
+                if (isValidPatter(pattern)) {
+                    System.out.println("Valid");
+                } else {
+                    System.out.println("Invalid");
+                }
             }
-          	
-            
+            testCases--;
+        }
+        in.close();
+    }
 
-		}
-	}
-
-    public static boolean isValidPatter(String pattern){
+    public static boolean isValidPatter(String pattern) {
         try {
             Pattern.compile(pattern);
             return true;
@@ -30,9 +30,4 @@ public class Solution
         }
     }
 
-
-
 }
-
-
-
