6ff29beb83747cf469c8f18baceae7c99e9eaf60
Dulangana Rupasinghe <127759019+Dula-cloud@users.noreply.github.com>
2026-08-12 19:32:04 +0530

    Update Solution.java

--- java/java regex/Solution.java
+++ java/java regex/Solution.java
@@ -2,11 +2,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.Scanner;
 
-class Solution{
+class Solution {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        while(in.hasNext()){
+        while (in.hasNext()) {
             String IP = in.next();
             System.out.println(IP.matches(new MyRegex().pattern));
         }
@@ -15,10 +15,7 @@ class Solution{
 }
 
 class MyRegex {
-
-    String pattern = ""
-
-    
+    String pattern = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
 }
 
-//Write your code here
+// Write your code here
